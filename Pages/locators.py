5d3f02b6f3833289dc1d7c708671bd0b1fcd82c5
Your Name <you@example.com>
2022-05-30 16:25:18 +0500

    Четвертый коммит. Поиск бага

--- Pages/locators.py
+++ Pages/locators.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     REGISTRATION_LINK = (By.CSS_SELECTOR, "#registration_link")
+    ADD_BUTTON = (By.CSS_SELECTOR, "button[type=submit]")
 
 
 class LoginPageLocators():    
@@ -14,3 +15,11 @@ class LoginPageLocators():
     REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
     REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTER_PASSWORD_REPEAT = (By.CSS_SELECTOR, "#id_registration-password2")
+
+
+class ProductPageLocators():
+    BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, ".product_main form button.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
+    PRODUCT_DESCRIPTION = (By.CSS_SELECTOR, "#product_description+p")
+    SUCCESS_MESSAGES = (By.CSS_SELECTOR, ".alertinner strong")
